599e05a4f55e4cd94848b5fa99222211a9c7879d
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-28 11:12:47 -0400

    Initial forecasting testing

---
+++ plot.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 15:20:21 2018
+
+@author: BJoseph
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import predict
+import pandas as pd
+
+app = dash.Dash()
+df = predict.predict()
+
+
+app.layout = html.Div(children = [
+        html.H1('Dash Tutorials'),
+        dcc.Graph(id='example',
+                  figure ={
+                          'data': [
+                                  {'x': df.index, 'y': df['Predicted Demand'], 'type': 'line', 'name': 'Demand'}
+                          ],
+                          'layout': {
+                                  'title': 'Basic Dash Example'
+                                  }
+                  })
+        ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
