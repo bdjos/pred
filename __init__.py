87c68fa5bd79d1a097189d4da16f42221ab37964
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-29 14:49:42 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import os
 
 PATH = os.getcwd()
 app = dash.Dash()
-df = pd.read_csv(f'{PATH}\predictions.csv', index_col='Date/Time')
+df = pd.read_csv(f'{PATH}/predictions.csv', index_col='Date/Time')
 
 app.layout = html.Div(children=[
     html.H1(children='Dash Tutorials'),
