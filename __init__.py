44cad3711a6b1628d44eaf7e92fabd014a334200
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-29 11:47:13 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,8 +4,9 @@ import dash_html_components as html
 import pandas as pd
 
 app = dash.Dash()
-x = [1,2,3]
-y = [10,11,12]
+df = pd.read_csv('predictions.csv', index_col=['Date/Time']
+x = pd.index
+y = df['Predicted Demand']
 
 app.layout = html.Div(children=[
     html.H1(children='Dash Tutorials'),
@@ -25,4 +26,5 @@ app.layout = html.Div(children=[
 server = app.server
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    print(x)
+    print(y)
