40160bca7e9cc4de094f3d28e3c063c90229dfb5
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-29 12:41:37 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,30 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 15:20:21 2018
+
+@author: BJoseph
+"""
+
 import dash
+from dash.dependencies import Output, Event
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+import predict
 import pandas as pd
 
-app = dash.Dash()
+app = dash.Dash(__name__)
 df = pd.read_csv('predictions.csv', index_col=['Date/Time'])
-x = df.index
-y = df['Predicted Demand']
 
-app.layout = html.Div(children=[
-    html.H1(children='Dash Tutorials'),
-    dcc.Graph(
-        id='example',
-        figure={
-            'data': [
-                {'x': x.values, 'y': y.values, 'type': 'line', 'name': 'Boats'}
-            ],
-            'layout': {
-                'title': 'Basic Dash Example'
-            }
-        }
-    )
-])
+X = deque(maxlen = 20)
+Y = deque(maxlen = 20)
+X.append(1)
+Y.append(1)
+
+app.layout = html.Div(children = [
+        html.H1('IESO Prediction'),
+        dcc.Graph(id='live-graph',
+                  animate=True),        
+        dcc.Interval(
+                id='graph-update',
+                interval=1000
+                )
+        ])
+
+@app.callback(Output('live-graph', 'figure'),
+              events = [Event('graph-update', 'interval')])
+def update_graph():
+    global X
+    global Y
+    X.append(X[-1] + 1)
+    Y.append(Y[-1] + (Y[-1]*random.uniform(-0.1, 0.1)))
+    
+    data = go.Scatter(
+            x = list(X),
+            y = list(Y),
+            name = 'Scatter',
+            mode = 'lines+markers'
+            )
+    return {'data': [data], 'layout': go.Layout(xaxis = dict(range=[min(X), max(X)]),
+                                                yaxis = dict(range=[min(Y), max(Y)]))}
+
 
 server = app.server
 
 if __name__ == '__main__':
-    print(x)
-    print(y)
+    app.run_server(debug=True)
