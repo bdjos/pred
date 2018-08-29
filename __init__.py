ce40d9dc727a6af284bb1fc4ea2971b7ee7afa9f
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-29 11:31:37 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,39 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 23 15:20:21 2018
-
-@author: BJoseph
-"""
-
 import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import predict
-import pandas as pd
 
 app = dash.Dash()
-df = pd.read_csv('predictions.csv', index_col=['Date/Time'])
 
-
-app.layout = html.Div(children = [
-        html.H1('IESO Prediction'),
-        dcc.Graph(id='example',
-                  figure ={
-                          'data': [
-                                  {'x': df.index, 'y': df['Predicted Demand'], 'type': 'line', 'name': 'Demand'}
-                          ],
-                          'layout': {
-                                  'title': 'Basic Dash Example'
-                                  }
-                  })
-        ])
-
-#@app.callback(
-#        Output(component_id='output', component_property='children'),
-#        [Input(component_id='input', component_property='value')])
-#def update_value(input_data):
-#    return "Input: {}.format(input_data)"
+app.layout = html.Div(children=[
+    html.H1(children='Dash Tutorials'),
+    dcc.Graph(
+        id='example',
+        figure={
+            'data': [
+                {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 2, 1, 5], 'type': 'line', 'name': 'Boats'},
+                {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},
+            ],
+            'layout': {
+                'title': 'Basic Dash Example'
+            }
+        }
+    )
+])
 
 server = app.server
 
