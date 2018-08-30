cecfc1e5c3d58a122d78cf0b0ee17b0e85118761
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-30 11:41:52 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -7,8 +7,9 @@ import pandasdb
 
 db_name = 'bjos'
 table_name = 'forecast'
+host = '167.99.178.209'
 
-db = pandasdb.pandasdb(db_name, table_name)
+db = pandasdb.pandasdb(db_name, table_name, host)
 df = db.pd_from_db()
 
 app.layout = html.Div(children=[
--- pandasdb.py
+++ pandasdb.py
@@ -8,9 +8,10 @@ import psycopg2
 import pandas as pd
 
 class pandasdb():
-    def __init__(self, database, table):
+    def __init__(self, database, table, host):
         self.database = database
         self.table = table
+        self.host = host
         
     def pd_to_db(self, dtypes, df, if_exists):
         if len(dtypes) != len(df.columns):
@@ -18,12 +19,12 @@ class pandasdb():
         else:
             dtypes_dict = dict(zip(list(df.columns), dtypes))
         
-            connection = psycopg2.connect(host="localhost", database="bjos", user="bjos", password="3iRM7Ihr@")
+            connection = psycopg2.connect(host=self.host, database="bjos", user="bjos", password="3iRM7Ihr@")
             df.to_sql(name = self.table, con = connection, if_exists = if_exists, dtype = dtypes_dict)
             connection.close()
         
     def pd_from_db(self):
-        connection = psycopg2.connect(host="localhost", database="bjos", user="bjos", password="3iRM7Ihr@")
+        connection = psycopg2.connect(host=self.host, database="bjos", user="bjos", password="3iRM7Ihr@")
         
         df = pd.read_sql(f'SELECT * FROM {self.table}', con = connection)
         connection.close()
