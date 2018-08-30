1a04287408c18efb2080a3e8a5617ec4bc37d959
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-30 11:28:39 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -3,18 +3,21 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import os
+import pandasdb
 
-PATH = '/var/www/FlaskApp/pred/predictions.csv'
-app = dash.Dash()
-df = pd.read_csv(PATH, index_col='Date/Time')
+db_name = 'bjos'
+table_name = 'forecast'
+
+db = pandasdb.pandasdb(db_name, table_name)
+df = db.pd_from_db()
 
 app.layout = html.Div(children=[
-    html.H1(children='Dash Tutorials'),
+    html.H1(children='Ontario Demand Prediction'),
     dcc.Graph(
         id='example',
         figure={
             'data': [
-                {'x': df.index, 'y': df['Predicted Demand'], 'type': 'line', 'name': 'Boats'},
+                {'x': df['Date/Time', 'y': df['Predicted Demand'], 'type': 'line', 'name': 'Predicted Demand'},
             ],
             'layout': {
                 'title': 'Basic Dash Example'
---
+++ pandasdb.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 22 15:24:19 2018
+
+@author: BJoseph
+"""
+import psycopg2
+import pandas as pd
+
+class pandasdb():
+    def __init__(self, database, table):
+        self.database = database
+        self.table = table
+        
+    def pd_to_db(self, dtypes, df, if_exists):
+        if len(dtypes) != len(df.columns):
+            print('len of dtypes must equal number of columns')
+        else:
+            dtypes_dict = dict(zip(list(df.columns), dtypes))
+        
+            connection = psycopg2.connect(host="localhost", database="bjos", user="bjos", password="3iRM7Ihr@")
+            df.to_sql(name = self.table, con = connection, if_exists = if_exists, dtype = dtypes_dict)
+            connection.close()
+        
+    def pd_from_db(self):
+        connection = psycopg2.connect(host="localhost", database="bjos", user="bjos", password="3iRM7Ihr@")
+        
+        df = pd.read_sql(f'SELECT * FROM {self.table}', con = connection)
+        connection.close()
+        
+        return df
+    
